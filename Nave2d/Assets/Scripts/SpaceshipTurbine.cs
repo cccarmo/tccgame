3722f8e83ea0b31db45332d1eedbc96e62213c8e
Daniel <danpaulalves@gmail.com>
2015-07-08 07:01:32 +0200

    Improved desing by setting layers.

---
+++ Nave2d/Assets/Scripts/SpaceshipTurbine.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpaceshipTurbine : MonoBehaviour {
+	private GameObject spaceship;
+
+	// Use this for initialization
+	void Start() {
+		spaceship = transform.parent.gameObject;
+		particleSortingLayerFix();
+	}
+	
+	// Update is called once per frame
+	void Update() {
+	
+	}
+
+	private void particleSortingLayerFix() {
+		ParticleSystem turbine = GetComponent<ParticleSystem>();
+		string spaceshipLayer = spaceship.GetComponent<Renderer>().sortingLayerName;
+		turbine.GetComponent<Renderer>().sortingLayerName = spaceshipLayer;
+		turbine.GetComponent<ParticleSystem>().GetComponent<Renderer>().sortingOrder = -1;
+	}
+}
