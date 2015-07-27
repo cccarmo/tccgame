86f9850fef5e9527f27bd1482fd24ca27c1c2907
Rafael Carmo <cccarmo@gmail.com>
2015-07-27 14:39:14 +0200

    Adding welcome scene

--- Nave2d/Assets/Scripts/LevelScripts/Prototype.cs
+++ Nave2d/Assets/Scripts/LevelScripts/Prototype.cs
@@ -2,11 +2,10 @@
 using System.Collections;
 
 public class Prototype : MonoBehaviour {
-
-	public GameObject gameScreen;
-
+	
 	// Use this for initialization
 	void Start () {
+
 	}
 	
 	// Update is called once per frame
---
+++ Nave2d/Assets/Scripts/ScreenTransition.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScreenTransition : MonoBehaviour {
+
+	private GameObject panelFade;
+
+	void Start () {
+		panelFade = GameObject.Find ("PanelFade");
+	}
+
+	//Transition
+	public void LoadNextLevel(string name)
+	{
+		StartCoroutine (LevelLoad (name));
+		panelFade.GetComponent<Animator>().Play ("FadeOut");
+	}
+	
+	IEnumerator LevelLoad(string name)
+	{
+		yield return new WaitForSeconds(0.333f);
+		Application.LoadLevel(name);
+	}
+
+
+}
