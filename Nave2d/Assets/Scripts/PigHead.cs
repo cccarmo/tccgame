28b8274c2799b999b22d44403d40ce956136fbc1
Rafael Carmo <cccarmo@gmail.com>
2015-07-27 16:46:51 +0200

    Adding pig head

---
+++ Nave2d/Assets/Scripts/PigHead.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PigHead : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void Talk() {
+		GetComponents<Animator>()[0].Play("PigTalk");
+		GetComponent<AudioSource> ().Play ();
+		Animator pigEye = GameObject.Find ("EyeRight").GetComponent<Animator>();
+		pigEye.Play ("PigBlink");
+		pigEye = GameObject.Find ("EyeLeft").GetComponent<Animator>();
+		pigEye.Play ("PigBlink");
+	}
+}
