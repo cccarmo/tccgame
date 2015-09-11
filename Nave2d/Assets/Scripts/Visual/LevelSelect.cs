5986acd9d3e255b6c3940be2893b52835173cd84
Rafael Carmo <cccarmo@gmail.com>
2015-09-11 16:32:57 -0300

    Adding LevelSelect

---
+++ Nave2d/Assets/Scripts/Visual/LevelSelect.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelSelect : MonoBehaviour {
+
+	private int module;
+
+	public void setModule (int m) {
+		module = m;
+	}
+
+	public void selectLevel (int level) {
+		LevelController.setModuleAndLevel (module, level);
+	}
+}
