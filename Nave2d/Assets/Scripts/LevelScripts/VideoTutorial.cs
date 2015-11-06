b470266cfe72a3954d54dea058575afac1bead0e
Felipe Yamaguti <yamaguti.felipe@gmail.com>
2015-11-05 23:21:00 -0200

    Adding Video Tutorials.

---
+++ Nave2d/Assets/Scripts/LevelScripts/VideoTutorial.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VideoTutorial : MonoBehaviour {
+
+	void Start () {
+		MovieTexture movie = ((MovieTexture)GetComponent<Renderer> ().material.mainTexture);
+		movie.loop = true;
+		movie.Play();
+	}
+}
