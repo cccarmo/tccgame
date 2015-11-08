f765ca52bd8ab7ca9f34b73f76767c00257734e7
Felipe Yamaguti <yamaguti.felipe@gmail.com>
2015-11-08 01:02:07 -0200

    Fixing crash on Shield State Machine Callbacks.

--- Nave2d/Assets/Scripts/Visual/DeactivateShieldBehavior.cs
+++ Nave2d/Assets/Scripts/Visual/DeactivateShieldBehavior.cs
@@ -5,20 +5,28 @@ public class DeactivateShieldBehavior : StateMachineBehaviour {
 
 	// OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
 	override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
-		GameObject Shield = GameObject.FindWithTag("Shield");
-		SpriteRenderer sprite = Shield.GetComponent<SpriteRenderer> ();
-		
-		Color opaque = new Color(1f,1f,1f,1f);
-		sprite.color = opaque;
+		if (GameObject.FindWithTag ("Shield") != null) {
+			GameObject Shield = GameObject.FindWithTag ("Shield");
+			if (Shield.GetComponent<SpriteRenderer> () != null) {
+				SpriteRenderer sprite = Shield.GetComponent<SpriteRenderer> ();
+			
+				Color opaque = new Color (1f, 1f, 1f, 1f);
+				sprite.color = opaque;
+			}
+		}
 	}
 
 
 	// OnStateExit is called when a transition ends and the state machine finishes evaluating this state
 	override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
-		GameObject Shield = GameObject.FindWithTag("Shield");
-		SpriteRenderer sprite = Shield.GetComponent<SpriteRenderer> ();
-		
-		Color transparent = new Color(1f,1f,1f,0f);
-		sprite.color = transparent;
+		if (GameObject.FindWithTag ("Shield") != null) {
+			GameObject Shield = GameObject.FindWithTag ("Shield");
+			if (Shield.GetComponent<SpriteRenderer> () != null) {
+				SpriteRenderer sprite = Shield.GetComponent<SpriteRenderer> ();
+			
+				Color transparent = new Color (1f, 1f, 1f, 0f);
+				sprite.color = transparent;
+			}
+		}
 	}
 }
