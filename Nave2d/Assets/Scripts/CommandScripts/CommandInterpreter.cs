b835f81ce6d51c07b7f089787630c6fd583f0fc0
Daniel <danpaulalves@gmail.com>
2015-11-08 04:00:17 +0100

    Fixed scrollable panel size.

--- Nave2d/Assets/Scripts/CommandScripts/CommandInterpreter.cs
+++ Nave2d/Assets/Scripts/CommandScripts/CommandInterpreter.cs
@@ -152,7 +152,7 @@ public class CommandInterpreter : DataRetriever {
 	}
 
 	private Vector3 calculateBoxPosition(GameObject box, int index, int nestLevel) {
-		int baseX = -310, baseY = 865, margin = 30;
+		int baseX = -310, baseY = 600, margin = 30;
 		float width = box.GetComponent<RectTransform>().rect.width;
 		return new Vector3(width/2 + baseX + margin + nestLevel * 50,
 		                   baseY - margin + index * -60, 0);
